0d7b1392c68c3dd0bcf699c7cbcf5217d0acc44c
Bosvark13 <arriec@gmail.com>
2019-05-13 08:49:42 +0200

    End of Section 3

---
+++ DatingApp.api/Controllers/AuthController.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using DatingApp.api.Data;
+using DatingApp.api.DTO;
+using DatingApp.api.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+
+namespace DatingApp.api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthRepository authRepository;
+        private readonly IConfiguration configuration;
+
+        public AuthController(IAuthRepository authRepository, IConfiguration configuration )
+        {
+            this.authRepository = authRepository;
+            this.configuration = configuration;
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register(UserForRegisterDto userForRegisterDto)
+        {
+            userForRegisterDto.Username = userForRegisterDto.Username.ToLower();
+
+            if (await authRepository.UserExists(userForRegisterDto.Username)) return BadRequest("Username already exists");
+
+            var user = new User()
+            {
+                Username = userForRegisterDto.Username
+            };
+
+            var newUser = await authRepository.Register(user, userForRegisterDto.Password);
+
+            return StatusCode(201);
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login(UserForLoginDto userForLoginDto)
+        {
+            var user = await authRepository.Login(userForLoginDto.Username.ToLower(), userForLoginDto.Password);
+
+            if (user == null) return Unauthorized();
+
+            var claims = new[]
+            {
+                new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
+                new Claim(ClaimTypes.Name, user.Username)
+            };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration.GetSection("AppSettings:Token").Value));
+
+            var credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+
+            var tokenDescriptor = new SecurityTokenDescriptor()
+            {
+              Subject = new ClaimsIdentity(claims),
+              Expires = DateTime.Now.AddDays(1),
+              SigningCredentials = credentials
+            };
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+
+            return Ok(new {
+                token = tokenHandler.WriteToken(token) 
+            });
+        }
+    }
+}
---
+++ DatingApp.api/Data/AuthRepository.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DatingApp.api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp.api.Data
+{
+    public class AuthRepository : IAuthRepository
+    {
+        private readonly DataContext dataContext;
+
+        public AuthRepository(DataContext dataContext)
+        {
+            this.dataContext = dataContext;
+        }
+
+        public async Task<User> Login(string username, string password)
+        {
+            var user = await dataContext.Users.FirstOrDefaultAsync(x => x.Username == username);
+
+            if (user == null) return null;
+
+            if (!VerifyPasswordHash(password, user.PasswordHash, user.PasswordSalt)) return null;
+
+            return user;
+        }
+
+        private bool VerifyPasswordHash(string password, byte[] passwordHash, byte[] passwordSalt)
+        {
+            using (var hmac = new System.Security.Cryptography.HMACSHA512(passwordSalt))
+            {
+                var computedHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+
+                for(int i = 0; i < computedHash.Length; i++)
+                {
+                    if (computedHash[i] != passwordHash[i]) return false;
+                }
+            }
+
+            return true;
+        }
+
+        public async Task<User> Register(User user, string password)
+        {
+            byte[] passworHash, passwordSalt;
+
+            CreatePasswordHash(password, out passworHash, out passwordSalt);
+
+            user.PasswordHash = passworHash;
+            user.PasswordSalt = passwordSalt;
+
+            await dataContext.Users.AddAsync(user);
+            await dataContext.SaveChangesAsync();
+
+            return user;
+        }
+
+        private void CreatePasswordHash(string password, out byte[] passworHash, out byte[] passwordSalt)
+        {
+            using (var hmac = new System.Security.Cryptography.HMACSHA512())
+            {
+                passwordSalt = hmac.Key;
+                passworHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+            }
+        }
+
+        public async Task<bool> UserExists(string username)
+        {
+            if (await dataContext.Users.AnyAsync(x => x.Username == username)) return true;
+
+            return false;
+        }
+    }
+}
