f0637d675ce7aa618fa907b8ba9c18307988444e
Bosvark13 <arriec@gmail.com>
2019-05-27 10:59:53 +0200

    End of Section 9

---
+++ DatingApp-SPA/src/app/members/member-card/member-card.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { User } from 'src/app/_models/user';
+
+@Component({
+    selector: 'app-member-card',
+    templateUrl: './member-card.component.html',
+    styleUrls: ['./member-card.component.scss']
+})
+/** member-card component*/
+export class MemberCardComponent implements OnInit {
+  @Input() user: User;  
+    /** member-card ctor */
+  constructor() {
+
+  }
+
+  ngOnInit(): void {
+    
+  }
+}
